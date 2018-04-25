e2076890f530607484c32de09b7b184f28fa0c48
avendrell <avendrell@192.168.56.1>
2018-04-25 16:46:14 +0200

    Version 2.1

--- src/prueba1/HolaVendrell.java
+++ src/prueba1/HolaVendrell.java
@@ -16,6 +16,7 @@ public class HolaVendrell {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Al mac no li entren virus.");
     }
     
 }
